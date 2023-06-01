a43b1e6ca507f087d64dacdadb61b54763bdafe3
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-06-01 17:29:56 +0200

    entering fixed Snack3

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -2,15 +2,15 @@ package org.lessons.java;
 
 public class Snack3 {
     public static void main(String[] args) {
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                sum += numbers[i];
-            }
+        int[] numbers= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int sum= 0;
+
+        for (int i = 1; i < numbers.length; i += 2) {
+            sum+= numbers[i];
         }
 
-        System.out.println("La somma dei numeri dispari da 1 a 20 è: " + sum);
+        System.out.println("La somma dei numeri in posizione dispari è: " + sum);
     }
 }
